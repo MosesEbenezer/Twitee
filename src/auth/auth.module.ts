9e556579e4aff0f9c62e11b15346437269c6bec2
Moses Ebenezer <moseschimdike@gmail.com>
2022-04-09 08:19:36 +0100

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthActions } from './entities/auth.entity';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthActions]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([AuthActions]),
+    UsersModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
